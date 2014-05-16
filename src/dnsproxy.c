758b030db68b9ad4a62b31d92207776aa6e495d6
fangdingjun <fangdingjun@gmail.com>
2014-05-16 16:19:19 +0800

    format code

--- src/dnsproxy.c
+++ src/dnsproxy.c
@@ -17,9 +17,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#ifdef USE_EPOLL /* epoll */
+#ifdef USE_EPOLL                /* epoll */
 #include <sys/epoll.h>
-#elif defined USE_SELECT  /* select */
+#elif defined USE_SELECT        /* select */
 #ifndef WIN32
 #include <sys/select.h>
 #endif
@@ -47,21 +47,21 @@
 #endif
 
 struct msg_data {
-    int listen_fd; /* the socket fd to listen */
-    struct sockaddr_in client_addr;  /* client socket address */
-    char msg_buf[512];   /* message buffer from client */
-    int msg_len;   /* the message length */
-    int status;   /* status, 0 for unused, 1 for used */
-    int srv_fd;   /* the server socket fd */
-    int fd;       /* listen fd or server fd */
-    time_t last_active;   /* the time for request in */
+    int listen_fd;              /* the socket fd to listen */
+    struct sockaddr_in client_addr; /* client socket address */
+    char msg_buf[512];          /* message buffer from client */
+    int msg_len;                /* the message length */
+    int status;                 /* status, 0 for unused, 1 for used */
+    int srv_fd;                 /* the server socket fd */
+    int fd;                     /* listen fd or server fd */
+    time_t last_active;         /* the time for request in */
 };
 
 char *listen_ip = "127.0.0.1";
 
 int listen_port = 53;
 
-char *servers[]={
+char *servers[] = {
     "8.8.8.8",
     "114.114.114.114",
     "208.67.222.222",
@@ -72,7 +72,8 @@ char *servers[]={
 /* the ip lists that china GFW used for DNS cache pollution 
 *  http://zh.wikipedia.org/wiki/%E5%9F%9F%E5%90%8D%E6%9C%8D%E5%8A%A1%E5%99%A8%E7%BC%93%E5%AD%98%E6%B1%A1%E6%9F%93
 */
-char *blacklist="1.1.1.1,255.255.255.255,74.125.127.102,74.125.155.102,74.125.39.102,74.125.39.113,209.85.229.138,4.36.66.178,8.7.198.45,37.61.54.158,46.82.174.68,59.24.3.173,64.33.88.161,64.33.99.47,64.66.163.251,65.104.202.252,65.160.219.113,66.45.252.237,72.14.205.104,72.14.205.99,78.16.49.15,93.46.8.89,128.121.126.139,159.106.121.75,169.132.13.103,192.67.198.6,202.106.1.2,202.181.7.85,203.161.230.171,203.98.7.65,207.12.88.98,208.56.31.43,209.145.54.50,209.220.30.174,209.36.73.33,209.85.229.138,211.94.66.147,213.169.251.35,216.221.188.182,216.234.179.13,243.185.187.3,243.185.187.39";
+char *blacklist =
+    "1.1.1.1,255.255.255.255,74.125.127.102,74.125.155.102,74.125.39.102,74.125.39.113,209.85.229.138,4.36.66.178,8.7.198.45,37.61.54.158,46.82.174.68,59.24.3.173,64.33.88.161,64.33.99.47,64.66.163.251,65.104.202.252,65.160.219.113,66.45.252.237,72.14.205.104,72.14.205.99,78.16.49.15,93.46.8.89,128.121.126.139,159.106.121.75,169.132.13.103,192.67.198.6,202.106.1.2,202.181.7.85,203.161.230.171,203.98.7.65,207.12.88.98,208.56.31.43,209.145.54.50,209.220.30.174,209.36.73.33,209.85.229.138,211.94.66.147,213.169.251.35,216.221.188.182,216.234.179.13,243.185.187.3,243.185.187.39";
 
 #ifdef USE_EPOLL
 int epfd;
@@ -98,12 +99,14 @@ int recv_from_client(struct msg_data *d)
         perror("recv from client");
         memset(d, 0, sizeof(struct msg_data));
         return -1;
-    }else if (d->msg_len == 0){
+    } else if (d->msg_len == 0) {
         memset(d, 0, sizeof(struct msg_data));
         return 0;
     }
 #ifdef DEBUG
-    printf("recv %d bytes from %s:%d\n", d->msg_len, inet_ntoa(d->client_addr.sin_addr), ntohs(d->client_addr.sin_port));
+    printf("recv %d bytes from %s:%d\n", d->msg_len,
+           inet_ntoa(d->client_addr.sin_addr),
+           ntohs(d->client_addr.sin_port));
 #endif
     send_to_server(d);
     return 0;
@@ -124,16 +127,16 @@ int send_to_server(struct msg_data *d)
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(53);
-    for(i=0;;i++){
-        if (servers[i] == NULL){
+    for (i = 0;; i++) {
+        if (servers[i] == NULL) {
             break;
         }
 
         saddr.sin_addr.s_addr = inet_addr(servers[i]);
 
         if (sendto
-                (d->srv_fd, d->msg_buf, d->msg_len, 0, (struct sockaddr *) &saddr,
-                 sizeof(saddr)) < 0) {
+            (d->srv_fd, d->msg_buf, d->msg_len, 0,
+             (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
             perror("sendto server");
             close(d->srv_fd);
             memset(d, 0, sizeof(struct msg_data));
@@ -167,38 +170,38 @@ int recv_from_server(struct msg_data *d)
         close(d->srv_fd);
         memset(d, 0, sizeof(struct msg_data));
         return -1;
-    }else if (msg_len == 0){ /* connection closed */
+    } else if (msg_len == 0) {  /* connection closed */
         close(d->srv_fd);
         memset(d, 0, sizeof(struct msg_data));
         return 0;
     }
 
     /* parse the dns message and check the black list */
-    do{
+    do {
         struct dns_msg *m;
         struct dns_rr *rr;
         char *ip;
         char *ip_found;
         int found = 0;
 
-        m=(struct dns_msg *)malloc(sizeof(struct dns_msg));
-        if(m == NULL){
+        m = (struct dns_msg *) malloc(sizeof(struct dns_msg));
+        if (m == NULL) {
             break;
         }
         memset(m, 0, sizeof(struct dns_msg));
         m->buf = malloc(msg_len);
-        if(m->buf == NULL){
+        if (m->buf == NULL) {
             free(m);
             break;
         }
         memcpy(m->buf, buf, msg_len);
         m->msg_len = msg_len;
         parse_msg(m);
-        for(rr = m->an; rr; rr=rr->next){
-            if ( rr->type == RR_A){
-                ip = rr->rdata; 
+        for (rr = m->an; rr; rr = rr->next) {
+            if (rr->type == RR_A) {
+                ip = rr->rdata;
                 ip_found = strstr(blacklist, ip);
-                if (ip_found != NULL){  /* ip is in blacklist */
+                if (ip_found != NULL) { /* ip is in blacklist */
                     found = 1;
 #ifdef DEBUG
                     printf("found bad ip %s, continue.\n", ip);
@@ -209,10 +212,10 @@ int recv_from_server(struct msg_data *d)
         }
         free_dns_msg(m);
 
-        if(found){
+        if (found) {
             return -1;
         }
-    }while(0);
+    } while (0);
 #ifdef DEBUG
     printf("recv %d bytes from server\n", msg_len);
 #endif
@@ -239,7 +242,8 @@ int send_to_client(struct msg_data *d, char *msg, int msg_len)
     epoll_ctl(epfd, EPOLL_CTL_DEL, d->srv_fd, &ev);
 #endif
 #ifdef DEBUG
-    printf("send to %s:%d\n", inet_ntoa(d->client_addr.sin_addr), htons(d->client_addr.sin_port));
+    printf("send to %s:%d\n", inet_ntoa(d->client_addr.sin_addr),
+           htons(d->client_addr.sin_port));
 #endif
     close(d->srv_fd);
     memset(d, 0, sizeof(struct msg_data));
@@ -271,7 +275,7 @@ int main(int argc, char *argv[])
 #ifdef WIN32
     /* initial the win32 sockets */
     WSADATA wsaData;
-    WSAStartup(0x2020,&wsaData);
+    WSAStartup(0x2020, &wsaData);
 #endif
 
     memset(msg, 0, sizeof(msg));
@@ -317,12 +321,12 @@ int main(int argc, char *argv[])
     int nr_events;
     int i;
     int j;
-    
+
     /* starting the loop */
     while (1) {
-#ifdef USE_EPOLL 
+#ifdef USE_EPOLL
         nr_events = epoll_wait(epfd, events, MAX_QUEUE + 1, 3000);
-#else  /* select */
+#else                           /* select */
         tv.tv_sec = 3;
         tv.tv_usec = 0;
         FD_ZERO(&rfds);
@@ -376,7 +380,7 @@ int main(int argc, char *argv[])
             msg[j].listen_fd = listen_fd;
             recv_from_client(&msg[j]);
         } else {                /* server response */
-#ifdef USE_EPOLL 
+#ifdef USE_EPOLL
             recv_from_server(d);
 #else
             for (i = 0; i < MAX_QUEUE; i++) {
@@ -386,13 +390,13 @@ int main(int argc, char *argv[])
                 if (FD_ISSET(msg[i].fd, &rfds)) {
                     recv_from_server(&msg[i]);
                 }
-            } /* for */
+            }                   /* for */
 #endif
-        } /* if */
+       }                        /* if */
 #ifdef USE_EPOLL
-     }  /* for */
+      }                         /* for */
 #endif
-    } /* while */
+    }                           /* while */
 
     return 0;
 }
