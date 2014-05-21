9331b7b4c5f7ea4d7f1ee71f6be112dd21ec8cd5
fangdingjun <fangdingjun@gmail.com>
2014-05-21 16:51:47 +0800

    re-write configure file parser

--- src/parse_cfg.c
+++ src/parse_cfg.c
@@ -28,6 +28,110 @@ char *skip_space_at_end(char *p)
     return p1;
 }
 
+int split_two_parts(char *str, int comma, char **part1, char **part2)
+{
+    char *p1;
+    char *p2;
+    char *p3;
+    int l;
+
+    /* eat space at beginning */
+    p1 = str;
+    p1 = skip_space_at_begin(p1);
+
+    p2 = strchr(p1, comma);
+
+    /* no comma found */
+    if (!p2) {
+        p2 = strchr(p1, '\0');
+    }
+
+    p3 = p2;
+
+    p2--;
+    p2 = skip_space_at_end(p2);
+
+    l = p2 - p1;
+    if (l) {
+        *part1 = malloc(l + 1);
+        if (*part1) {
+            strncpy(*part1, p1, l);
+            (*part1)[l] = '\0';
+        }
+    } else {
+        *part1 = NULL;
+    }
+
+    if (*p3 == '\0') {
+        *part2 = NULL;
+        return 0;
+    }
+    p3++;
+    p1 = p3;
+    p1 = skip_space_at_begin(p1);
+    p2 = strchr(p1, '\0');
+    p2--;
+    p2 = skip_space_at_end(p2);
+
+    l = p2 - p1;
+    if (l) {
+        *part2 = malloc(l + 1);
+        if (*part2) {
+            strncpy(*part2, p1, l);
+            (*part2)[l] = '\0';
+        }
+    } else {
+        *part2 = NULL;
+    }
+
+    return 0;
+}
+
+char **split(char *str, int comma)
+{
+    char **arr;
+    char *p;
+    int pos = 0;
+    int maxsize;
+
+    maxsize = 100;
+
+    arr = malloc(maxsize * sizeof(char *));
+    if (!arr) {
+        fprintf(stderr, "out of memory\n");
+        return NULL;
+    }
+    memset(arr, 0, maxsize * sizeof(char *));
+    p = malloc(strlen(str) + 1);
+    if (!p) {
+        fprintf(stderr, "out of memory\n");
+        free(arr);
+        return NULL;
+    }
+    strcpy(p, str);
+    while (1) {
+        char *p1 = NULL;
+        char *p2 = NULL;
+        split_two_parts(p, comma, &p1, &p2);
+        free(p);
+        if (p1) {
+            arr[pos] = p1;
+            pos++;
+            if (pos == (maxsize - 1)) {
+                maxsize = maxsize << 1;
+                arr = realloc(arr, maxsize);
+            }
+        }
+        if (!p2)
+            break;
+        p = p2;
+    }
+    arr[pos] = NULL;
+    arr = realloc(arr, (pos + 1) * sizeof(char *));
+    return arr;
+}
+
+/* parse key-value configfile, use struct arg_map */
 int parse_cfg(char *filename, struct arg_map *args)
 {
     char *fn;
@@ -46,48 +150,39 @@ int parse_cfg(char *filename, struct arg_map *args)
     while (fgets(buf, 1024, fp) != NULL) {
         char *p1;
         char *p2;
-        char *p3;
+        //char *p3;
         char *k;
         char *v;
-        int l;
+        //int l;
         p1 = buf;
-        while (isspace(*p1))
-            p1++;
-        if (*p1 == '#')
-            continue;
-        if (*p1 == '\0')
-            continue;
-        //printf("%s",buf);
-        p2 = strchr(p1, '=');
-        if (!p2)
-            continue;
-        p3 = p2;
-        p2--;
 
-        p2 = skip_space_at_end(p2);
-
-        l = p2 - p1;
-        k = (char *) malloc(l + 1);
-        strncpy(k, p1, l);
-        k[l] = '\0';
-
-        //printf("get key: '%s'", k);
-
-        p3++;
-        p1 = p3;;
+        /* eat space */
         p1 = skip_space_at_begin(p1);
 
-        p2 = strchr(p1, '\r');
+        /* ignore comments */
+        if (*p1 == '#')
+            continue;
+
+        /* empty line, ignore */
+        if (*p1 == '\0')
+            continue;
+
+        p2 = strchr(p1, '=');
+
+        /* no '=' found, ignore */
         if (!p2)
-            p2 = strchr(p1, '\n');
-        if (!p2)
-            p2 = strchr(p1, '\0');
-        p2 = skip_space_at_end(p2);
-        l = p2 - p1;
-        v = (char *) malloc(l + 1);
-        strncpy(v, p1, l);
-        v[l] = '\0';
-        //printf(" = '%s'\n", v);
+            continue;
+
+        split_two_parts(p1, '=', &k, &v);
+
+        if (k == NULL || v == NULL) {
+            if (k)
+                free(k);
+            if (v)
+                free(v);
+            continue;
+        }
+        /* process argument map */
         int found = 0;
         for (i = 0; args[i].name != NULL; i++) {
             if (strcmp(args[i].name, k) == 0) {
@@ -101,44 +196,20 @@ int parse_cfg(char *filename, struct arg_map *args)
                     v = NULL;
                 } else if (args[i].type == ARG_STR_ARRARY) {
                     char **ap;
-                    char *ap1[10];
-                    //char *k1;
-                    char *pp1;
-                    char *pp2;
-                    char *pp3;
-                    int pos = 0;
-                    ap = (char **) malloc(sizeof(ap1));
-                    pp1 = v;
-                    while (1) {
-                        pp1 = skip_space_at_begin(pp1);
-                        pp2 = strchr(pp1, ',');
-                        if (!pp2)
-                            pp2 = strchr(pp1, '\0');
-                        pp3 = pp2;
-                        pp2--;
-                        pp2 = skip_space_at_end(pp2);
-                        l = pp2 - pp1;
-                        ap[pos] = (char *) malloc(l + 1);
-                        strncpy(ap[pos], pp1, l);
-                        ap[pos][l] = '\0';
-                        if (*pp3 == '\0')
-                            break;
-                        pp3++;
-                        pp1 = pp3;
-                        pos++;
-                        if (pos > 9)
-                            break;
-                    }
-                    ap[pos] = NULL;
+                    ap = split(v, ',');
                     *((char ***) (args[i].addr)) = ap;
                     free(v);
+                    v = NULL;
                 } else {
                     printf("unknown type\n");
                     free(v);
+                    v = NULL;
                 }
+
+                /* found one, goto out */
                 break;
-            }
-        }
+            }                   /* if strcmp */
+        }                       /* for */
         if (!found) {
             printf("unkown key: %s\n", k);
             free(k);
@@ -149,7 +220,7 @@ int parse_cfg(char *filename, struct arg_map *args)
             free(k);
             k = NULL;
         }
-    }
+    }                           /* while(fgets) */
     fclose(fp);
     return 0;
 }
@@ -165,6 +236,11 @@ int get_blackip(char *filename, char **ips)
         return -1;
     }
     buf1 = (char *) malloc(65536);
+    if(!buf1){
+        fprintf(stderr, "out of memory\n");
+        *ips = NULL;
+        return -1;
+    }
     fp = fopen(filename, "r");
     if (fp == NULL) {
         //perror(filename);
