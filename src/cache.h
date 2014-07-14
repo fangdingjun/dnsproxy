85c94e68f8806ec34c62124bec8e96ac0d715d43
fangdingjun <fangdingjun@gmail.com>
2014-07-14 11:32:05 +0800

    add function defination for new cache implement

--- src/cache.h
+++ src/cache.h
@@ -1,4 +1,4 @@
-
+#include <ldns/ldns.h>
 #ifndef CACHE_H
 #define CACHE_H
 
@@ -13,4 +13,8 @@ int cache_store(sqlite3 * db, char *tbl_name, struct dns_rr *r);
 struct dns_rr *cache_fetch(sqlite3 * db, char *tbl_name, char *qname,
                            int qtype);
 int delete_expired(sqlite3 * db, char *tbl_name);
+int cache_rr(ldns_rr_list * r, sqlite3 * db, char *tblname);
+ldns_rr_list *lookup_cache(ldns_rr * q, sqlite3 * db, char *tblname);
+sqlite3 *cache_init(char *filename, char *tblname, size_t cache_size);
+int delete_expired_rr(sqlite3 * db, char *tblname);
 #endif
